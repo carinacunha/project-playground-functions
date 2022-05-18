090dce6af0d340fa530a4eb3f2fbc81c47897106
Carina Cunha <carina.cunha13@gmail.com>
2022-05-18 18:26:47 -0300

    Desafio 8

--- src/challenges.js
+++ src/challenges.js
@@ -1,48 +1,62 @@
 // Desafio 1
 function compareTrue(first, second) {
   // seu código aqui
-  if(first && second == true){
+  if (first && second === true) {
     return true;
-  }
-    return false;
-}
+  };
+  return false;
+};
 
 // Desafio 2
 function calcArea(base, height) {
   // seu código aqui
   return (base * height) / 2;
-}
+};
 
 // Desafio 3
 function splitSentence(array) {
   // seu código aqui
   return array.split(' ');
-}
+};
 
 // Desafio 4
 function concatName(array) {
   // seu código aqui
-  firstString = array[0];
-  lastString = array.pop();
-  string = lastString +', '+ firstString;
+  let firstString = array[0];
+  let lastString = array.pop();
+  let string = lastString +', '+ firstString;
 
   return string;
-}
+};
 
 // Desafio 5
 function footballPoints(wins, ties) {
   // seu código aqui
   let sum = (wins * 3) + (ties * 1);
   return sum;
-}
+};
 
 // Desafio 6
-function highestCount() {
+function highestCount(array) {
   // seu código aqui
-}
+  let biggest;
+  let countBiggest = 0;
+  for(index = 0; index < array.length; index += 1) {
+    if(Math.max(array[index], array[index + 1]) || Math.max(array[index]), array[index + 1]){
+      biggest = array[index];
+    };
+  };
+  
+  for(index = 0; index <= array.length; index += 1) {
+    if(array[index] === biggest){
+        countBiggest += 1;
+    };
+  };
+ return countBiggest;
+};
 
 // Desafio 7
-function catAndMouse(mouse, cat1, cat2){
+function catAndMouse(mouse, cat1, cat2) {
   //seu código aqui
 
   let distance1 = Math.abs(mouse - cat1);
@@ -51,19 +65,41 @@ function catAndMouse(mouse, cat1, cat2){
   if(distance1 < distance2){
     return 'cat1'
   }
-  else if(distance2 < distance1){
+  if(distance2 < distance1){
     return 'cat2'
   }
-
-  else if(distance2 === distance1){
+  if(distance2 === distance1){
     return 'os gatos trombam e o rato foge'
   }
 }
 
-
 // Desafio 8
-function fizzBuzz() {
+function fizzBuzz(array) {
   // seu código aqui
+
+let three = [];
+let five = [];
+let result = [];
+
+for(key in array){
+  if (array[key] % 3 === 0 && array[key] % 5 !== 0) {
+    three.push(array[key] / 3);
+    result.push('fizz');
+  }
+  else if (array[key] % 5 === 0 && array[key] % 3 !== 0) {
+    five.push(array[key] / 5);
+    result.push('buzz');
+  }
+  else if (array[key] % 5 === 0 && array[key] % 3 === 0) {
+    five.push(array[key] / 5);
+    three.push(array[key] / 3);
+    result.push('fizzBuzz');
+  }
+  else{
+    result.push('bug!');
+  }  
+}
+  return result;
 }
 
 // Desafio 9
