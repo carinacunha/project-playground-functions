a7c05042f825a663d07e5d5dea07abb93bd1c9f4
Carina Cunha <carina.cunha13@gmail.com>
2022-05-18 22:09:54 -0300

    Desafio 9

--- src/challenges.js
+++ src/challenges.js
@@ -3,21 +3,21 @@ function compareTrue(first, second) {
   // seu código aqui
   if (first && second === true) {
     return true;
-  };
+  }
   return false;
-};
+}
 
 // Desafio 2
 function calcArea(base, height) {
   // seu código aqui
   return (base * height) / 2;
-};
+}
 
 // Desafio 3
 function splitSentence(array) {
   // seu código aqui
   return array.split(' ');
-};
+}
 
 // Desafio 4
 function concatName(array) {
@@ -27,14 +27,14 @@ function concatName(array) {
   let string = lastString +', '+ firstString;
 
   return string;
-};
+}
 
 // Desafio 5
 function footballPoints(wins, ties) {
   // seu código aqui
   let sum = (wins * 3) + (ties * 1);
   return sum;
-};
+}
 
 // Desafio 6
 function highestCount(array) {
@@ -43,42 +43,40 @@ function highestCount(array) {
   let biggest = Math.max.apply(null, array);
   let countBiggest = 0;
 
-
-  for (index = 0; index <= array.length; index += 1) {
-      if(array[index] === biggest) {
-          countBiggest += 1;
-        }
+  for (let index = 0; index <= array.length; index += 1) {
+    if (array[index] === biggest) {
+      countBiggest += 1;
+    }
   }
-    return countBiggest;
-  }
+return countBiggest;
+}
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  //seu código aqui
+// seu código aqui
 
   let distance1 = Math.abs(mouse - cat1);
   let distance2 = Math.abs(mouse - cat2);
 
-  if(distance1 < distance2){
-    return 'cat1'
+  if (distance1 < distance2) {
+    return 'cat1';
   }
-  if(distance2 < distance1){
-    return 'cat2'
+  if (distance2 < distance1) {
+    return 'cat2';
   }
-  if(distance2 === distance1){
-    return 'os gatos trombam e o rato foge'
+  if (distance2 === distance1) {
+    return 'os gatos trombam e o rato foge';
   }
 }
 
 // Desafio 8
 function fizzBuzz(array) {
-  // seu código aqui
-
+// seu código aqui
   let three = [];
   let five = [];
   let result = [];
 
-  for(key in array){
+  for (let key in array) {
     if (array[key] % 3 === 0 && array[key] % 5 !== 0) {
       three.push(array[key] / 3);
       result.push('fizz');
@@ -92,25 +90,80 @@ function fizzBuzz(array) {
       three.push(array[key] / 3);
       result.push('fizzBuzz');
     }
-    else{
+    else {
       result.push('bug!');
     }  
   }
-    return result;
-  }
+return result;
+}
 
 // Desafio 9
-function encode(string) {
+function encode(words) {
+  // seu código aqui
+ let wordsEncode = words.split('');
+
+  for (let i = 0; i < words.length; i += 1) {
+
+      if (wordsEncode[i] === 'a') {
+          wordsEncode[i] = '1';
+      }
+
+      else if (wordsEncode[i] === 'e') {
+          wordsEncode[i] = '2';
+      }
+
+      else if (wordsEncode[i] === 'i') {
+          wordsEncode[i] = '3';
+      }
+          
+      else if (wordsEncode[i] === 'o') {
+          wordsEncode[i] = '4';
+      }
+
+      else if (wordsEncode[i] === 'u') {
+          wordsEncode[i] = '5';
+      }
+  }
+  wordsEncode = wordsEncode.join('');
+  return wordsEncode;
+} 
+
+
+function decode(words) {
   // seu código aqui
 
-}
-function decode() {
-  // seu código aqui
-}
+  let wordsDecode = words.split('');
+
+  for (let i = 0; i < words.length; i += 1) {
+
+    if (wordsDecode[i] === '1') {
+      wordsDecode[i] = 'a';
+    }
+
+    else if (wordsDecode[i] === '2') {
+      wordsDecode[i] = 'e';
+    }
+
+    else if (wordsDecode[i] === '3') {
+      wordsDecode[i] = 'i';
+    }
+        
+    else if (wordsDecode[i] === '4') {
+      wordsDecode[i] = 'o';
+    }
+
+    else if (wordsDecode[i] === '5') {
+      wordsDecode[i] = 'u';
+    }
+  }
+  wordsDecode = wordsDecode.join('');
+  return wordsDecode;
+} 
 
 // Desafio 10
-function techList() {
+function techList(array, string) {
   // seu código aqui
+
 }
 
 module.exports = {
--- src/challenges2.js
+++ src/challenges2.js
@@ -5,18 +5,18 @@ function generatePhoneNumber() {
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
-  // seu código aqui
+// seu código aqui
   let sum1 = lineA + lineB;
   let sum2 = lineA + lineC;
   let sum3 = lineB + lineC;
 
-  if (lineA < sum3 && lineA > Math.abs(lineB - lineC)){
+  if (lineA < sum3 && lineA > Math.abs(lineB - lineC)) {
     return true;
   }
-  else if (lineB < sum2 && lineB > Math.abs(lineA - lineC)){
+  else if (lineB < sum2 && lineB > Math.abs(lineA - lineC)) {
     return true;
   }
-  else if (lineC < sum1 && lineC > Math.abs(lineA - lineB)){
+  else if (lineC < sum1 && lineC > Math.abs(lineA - lineB)) {
     return true;
   }
   else{
